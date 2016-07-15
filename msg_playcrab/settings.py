48eba83b0332c641c3a678b6e32c904c7d6cdecc
playcrab <jiayuguang@qq.com>
2016-07-15 11:48:32 +0800

    xin

--- msg_playcrab/settings.py
+++ msg_playcrab/settings.py
@@ -14,7 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 # BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+#
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__))).replace('\\', '/')
 TEMPLATES_PATH = os.path.join(BASE_DIR, 'templates')
 
