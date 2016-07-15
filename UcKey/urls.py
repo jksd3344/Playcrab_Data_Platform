0284cb7fbfc719cb5c5b7ebe458c4d40d0a822f0
playcrab <jiayuguang@qq.com>
2016-07-15 11:11:57 +0800

    xin

---
+++ UcKey/urls.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+#coding=utf-8
+
+from django.conf.urls import url
+from django.contrib import admin
+
+
--- UcKey/views.py
+++ UcKey/views.py
@@ -1,3 +1,11 @@
+#!/usr/bin/python
+#coding=utf-8
+
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
 # Create your views here.
+
+
+class login_index(TemplateView):
+    pass
