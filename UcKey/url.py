849ae2fbb5d204ff7cb3486f39a67612d6887cd6
playcrab <jiayuguang@qq.com>
2016-07-15 11:56:32 +0800

    xin

--- UcKey/url.py
+++ UcKey/url.py
@@ -6,6 +6,6 @@ from django.contrib import admin
 
 from views import login_index
 urlpatterns = [
-    url(r'^login_index/', login_index.as_views(),name="login_index"),
+    url(r'^login_index/', login_index.as_view(),name="login_index"),
 
 ]
