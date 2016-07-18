3be42ea4b51a82ad981f17705705d154881dbeb3
playcrab <jiayuguang@qq.com>
2016-07-18 11:54:54 +0800

    xin

--- UcKey/tests.py
+++ UcKey/tests.py
@@ -20,9 +20,9 @@ class AccessApi(object):
     '''
     def __init__(self,current_method,data,url_path):
         self.settings = {
-            'api_url' : 'http://api.ucenter.playcrab.com/',
-            'api_key' : '3158308647',
-            'api_secret_key' : '454fceaa64e202256b295a7184272af4'
+            'api_url': 'http://119.254.111.26:8086/',
+            'api_key': '4190854055',
+            'api_secret_key': 'b08be6a42e7d86a7a57557dfc3afdd7b'
         }
         self.params = {
             'id': '1',
