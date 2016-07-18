d78ef3d29f9d4e6dbc6c0c6a6b35a0e63a9c45c0
playcrab <jiayuguang@qq.com>
2016-07-18 10:33:54 +0800

    xin

--- UcKey/views.py
+++ UcKey/views.py
@@ -7,17 +7,16 @@ from django.shortcuts import redirect
 from tests import Common
 
 
-class LoginView(RedirectView):
-
-    def get(self,request):
-        host=request.get_host()
-        callback_url= "http://%s/login/CheckView/"% host
-        Com=Common()
-        url=Com.loginUrl(callback_url)
-        return redirect(url['result'])
+# class LoginView(RedirectView):
+#
+#     def get(self,request):
+#         host=request.get_host()
+#         callback_url= "http://%s/login/CheckView/"% host
+#         Com=Common()
+#         url=Com.loginUrl(callback_url)
+#         return redirect(url['result'])
 
 
 class CheckView(RedirectView):
     template_name="UcKey/login_index.html"
-
 
--- urls/UcKeyurl.py
+++ urls/UcKeyurl.py
@@ -6,6 +6,6 @@ from django.contrib import admin
 
 from UcKey.views import *
 urlpatterns = [
-    url(r'^LoginView/', LoginView.as_view(),name="LoginView"),
+    # url(r'^LoginView/', LoginView.as_view(),name="LoginView"),
     url(r'^CheckView/',CheckView.as_view(),name="CheckView")
 ]
