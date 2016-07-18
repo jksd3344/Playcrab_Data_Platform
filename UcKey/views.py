5ea653802dc8bfb5b32e9362bf477bd0ba67fb98
playcrab <jiayuguang@qq.com>
2016-07-18 10:54:37 +0800

    xin

--- UcKey/views.py
+++ UcKey/views.py
@@ -7,14 +7,14 @@ from django.shortcuts import redirect
 from tests import Common
 
 
-# class LoginView(RedirectView):
-#
-#     def get(self,request):
-#         host=request.get_host()
-#         callback_url= "http://%s/login/CheckView/"% host
-#         Com=Common()
-#         url=Com.loginUrl(callback_url)
-#         return redirect(url['result'])
+class LoginView(RedirectView):
+
+    def get(self,request):
+        host=request.get_host()
+        callback_url= "http://%s/login/CheckView/"% host
+        Com=Common()
+        url=Com.loginUrl(callback_url)
+        return redirect(url['result'])
 
 
 class CheckView(TemplateView):
