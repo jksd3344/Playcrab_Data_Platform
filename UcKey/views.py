89ff0e8241da245cbed8097753d57993b17e57e3
playcrab <jiayuguang@qq.com>
2016-07-18 12:07:35 +0800

    xin

--- UcKey/views.py
+++ UcKey/views.py
@@ -4,7 +4,21 @@
 
 from django.views.generic import TemplateView,RedirectView
 from django.shortcuts import redirect
+from django.http import HttpResponse
 from tests import Common
+import json
+
+
+class JsonRes(HttpResponse):
+    def __init__(self,
+            content={},
+            status=None,
+            content_type='application/json'):
+
+        super(JsonRes, self).__init__(
+            json.dumps(content),
+            status=status,
+            content_type=content_type)
 
 
 class LoginView(RedirectView):
@@ -26,10 +40,5 @@ class CheckView(TemplateView):
         token = request.GET.get("token")
         Com=Common()
         data=Com.checkToken(token)
-        print("data=%s"%data)
+        JsonRes(json.dumps(data))
 
-
-    def get_context_data(self, **kwargs):
-        context = super(CheckView, self).get_context_data(**kwargs)
-        context['Node'] = 0
-        return context
